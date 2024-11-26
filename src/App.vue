5aefa893db11c38d95696becadecad7331ba0ddd
pedrodruviaro <pedrodruviaro@gmail.com>
2024-11-26 10:44:32 -0300

    feat: main layout and mock list

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,69 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+interface Card {
+  id: number
+  title: string
+  description: string
+}
+interface List {
+  id: number
+  title: string
+  cards: Card[]
+}
+
+const lists = reactive<List[]>([
+  {
+    id: 1,
+    title: 'To Do',
+    cards: [
+      { id: 1, title: 'Task 1', description: 'Description for Task 1' },
+      { id: 2, title: 'Task 2', description: 'Description for Task 2' },
+    ],
+  },
+  {
+    id: 2,
+    title: 'In Progress',
+    cards: [
+      { id: 3, title: 'Task 3', description: 'Description for Task 3' },
+      { id: 4, title: 'Task 4', description: 'Description for Task 4' },
+    ],
+  },
+  {
+    id: 3,
+    title: 'Done',
+    cards: [{ id: 5, title: 'Task 5', description: 'Description for Task 5' }],
+  },
+])
+</script>
 
 <template>
-  <h1 class="font-bold">My app</h1>
+  <main class="p-5 font-sans">
+    <div class="flex gap-5 py-5 overflow-x-auto">
+      <article
+        class="bg-gray-100 p-3 rounded-lg min-w-[250px] flex flex-col"
+        v-for="list in lists"
+        :key="list.id"
+      >
+        <h2 class="font-medium mb-2">{{ list.title }}</h2>
+
+        <div
+          class="bg-white p-2 my-2 rounded shadow cursor-pointer"
+          v-for="card in list.cards"
+          :key="card.id"
+        >
+          <span class="text-sm font-medium">{{ card.title }}</span>
+          <p class="text-xs text-gray-400">{{ card.description }}</p>
+        </div>
+
+        <div class="mt-auto">
+          <button
+            class="w-full bg-transparent hover:bg-white rounded text-gray-500 p-2 text-left mt-2 text-sm font-medium"
+          >
+            &plus; Add Card
+          </button>
+        </div>
+      </article>
+    </div>
+  </main>
 </template>
